7ecbc5d9011a6d0e3e5daa0d0e3a411e07ea22c7
abdelaziz gamal <84747851+abdelazizgamal@users.noreply.github.com>
2024-06-23 01:07:37 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,64 +1,14 @@
 import streamlit as st
-
 import subprocess
 import sys
-
-def install():
-    subprocess.call([sys.executable, '-m', 'pip', 'install', "pyyaml==5.1"])
-    # subprocess.call([sys.executable, '-m', 'pip', 'install', gitpython])
-    # from git import Repo  # pip install gitpython
-
-    # Repo.clone_from('https://github.com/facebookresearch/detectron2', repo_dir)
-
-    subprocess.call([sys.executable, '-m', 'pip', 'install', "git+https://github.com/facebookresearch/detectron2.git"])
-
-# # pip install pyyaml==5.1
-# import sys, os, distutils.core
-# # Note: This is a faster way to install detectron2 in Colab, but it does not include all functionalities (e.g. compiled operators).
-# # See https://detectron2.readthedocs.io/tutorials/install.html for full installation instructions
-# !git clone 'https://github.com/facebookresearch/detectron2'
-# dist = distutils.core.run_setup("./detectron2/setup.py")
-# !python -m pip install {' '.join([f"'{x}'" for x in dist.install_requires])}
-# sys.path.insert(0, os.path.abspath('./detectron2'))
-
-install()
-from detectron2.utils.logger import setup_logger
-setup_logger()
-
-# import some common libraries
-import numpy as np
-import os, json, cv2, random
-import detectron2
-# import some common detectron2 utilities
-from detectron2 import model_zoo
-from detectron2.engine import DefaultPredictor
-from detectron2.config import get_cfg
-from detectron2.utils.visualizer import Visualizer
-from detectron2.data import MetadataCatalog, DatasetCatalog
-
-
-# Properly install detectron2. (Please do not install twice in both ways)
-# !python -m pip install 'git+https://github.com/facebookresearch/detectron2.git'
 
 @st.cache(allow_output_mutation=True)
 
 def install():
     subprocess.call([sys.executable, '-m', 'pip', 'install', "pyyaml==5.1"])
-    # subprocess.call([sys.executable, '-m', 'pip', 'install', gitpython])
-    # from git import Repo  # pip install gitpython
-
-    # Repo.clone_from('https://github.com/facebookresearch/detectron2', repo_dir)
-
     subprocess.call([sys.executable, '-m', 'pip', 'install', "git+https://github.com/facebookresearch/detectron2.git"])
 
-# # pip install pyyaml==5.1
-# import sys, os, distutils.core
-# # Note: This is a faster way to install detectron2 in Colab, but it does not include all functionalities (e.g. compiled operators).
-# # See https://detectron2.readthedocs.io/tutorials/install.html for full installation instructions
-# !git clone 'https://github.com/facebookresearch/detectron2'
-# dist = distutils.core.run_setup("./detectron2/setup.py")
-# !python -m pip install {' '.join([f"'{x}'" for x in dist.install_requires])}
-# sys.path.insert(0, os.path.abspath('./detectron2'))
+
 with st.spinner('Model is being loaded..'):
   install()
 from detectron2.utils.logger import setup_logger
@@ -67,8 +17,6 @@ setup_logger()
 # import some common libraries
 import numpy as np
 import os, json, cv2, random
-from google.colab.patches import cv2_imshow
-
 
 import detectron2
 # import some common detectron2 utilities
@@ -78,33 +26,35 @@ from detectron2.config import get_cfg
 from detectron2.utils.visualizer import Visualizer
 from detectron2.data import MetadataCatalog, DatasetCatalog
 
-# def load_model():
-#   model=tf.keras.models.load_model('/content/my_model2.hdf5')
-#   return model
-
-#   model=load_model()
 
 st.write("""
          # Image_Analysis
          """
          )
-
-file = st.file_uploader("Please upload an brain scan file", type=["jpg", "png","jpeg"])
-
-from PIL import Image, ImageOps
-st.set_option('deprecation.showfileUploaderEncoding', False)
+#Load wegihts and model
 cfg = get_cfg()
-def import_and_predict(image_data):
 
+def setup_cfg():
       cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
       cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  # set threshold for this model
       # Find a model from detectron2's model zoo.  https://github.com/facebookresearch/detectron2/blob/main/MODEL_ZOO.md
       cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
+
+setup_cfg()
+
+#upload image
+file = st.file_uploader("Please upload an brain scan file", type=["jpg", "png","jpeg"])
+
+from PIL import Image, ImageOps
+st.set_option('deprecation.showfileUploaderEncoding', False)
+
+
+#Apply analysis to Image
+def import_and_predict(image_data):
       predictor = DefaultPredictor(cfg)
       outputs = predictor(image_data)
-
-
       return outputs
+
 if file is None:
     st.text("Please upload an image file")
 else:
@@ -113,11 +63,13 @@ else:
 
     st.image(image, use_column_width=True)
 
+    #Analysis
     with st.spinner('analyzing image..'):
+      
       predictions = import_and_predict(opencvImage)
     instances = predictions["instances"]
 
-    # We can use `Visualizer` to draw the predictions on the image.
+    # use `Visualizer` to draw the predictions on the image.
     v = Visualizer(opencvImage[:, :, ::-1], MetadataCatalog.get(cfg.DATASETS.TRAIN[0]), scale=0.8)
     out = v.draw_instance_predictions(instances.to("cpu"))
 
@@ -132,11 +84,11 @@ else:
     class_catalog = metadata.thing_classes
     pred_class_names = [class_catalog[idx] for idx in detected_class_indexes]
 
-    # insert the list to the set
+    #Get the unique Classes only
     list_set = set(pred_class_names)
-    # convert the set to the list
     unique_class_names = (list(list_set))
 
+    #Show List of Classes
     s = ''
     for i in unique_class_names:
         s += "- " + i + "\n"
